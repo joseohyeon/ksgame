9bfa629dde30ca7f5a8ef29d4ee140946c9e7798
joseohyeon <76928063+joseohyeon@users.noreply.github.com>
2021-01-05 16:38:25 +0900

    Add files via upload

---
+++ 0105-2/0105-2/0105-2.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int unit, count, total;
+    unit = 500;
+    count = 5;
+    total = unit * count;
+    cout << "총금액:" << total << endl;
+    
+    return 0;
+}
---
+++ 0105-5/0105-5/0105-5.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int a;
+
+	cout << "정수입력 : ";
+	cin >> a;
+
+	if (a > 100)
+		cout << a << "은 100보다 크다" << endl;
+	else
+		cout << a << "은 100보다 작거나 같다" << endl;
+
+	cout << "**************************" << endl;
+	
+	return 0;
+}
